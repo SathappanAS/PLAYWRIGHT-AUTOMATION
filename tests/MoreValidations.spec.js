6f40bebe30a0b4a4428e4dfa04a82c5ef5964c5a
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-04 17:24:34 -0700

    learning new functions

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -2,11 +2,20 @@ const {test,expect} = require ('@playwright/test')
 
 test("Popup validations", async({page})=>{
     await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+  
+    await page.locator('#mousehover').hover();
+
+
+
     //await page.goto('https://www.google.com/');
     //await page.goBack();
     //await page.goForward();
 
-    await expect(page.locator('#displayed-text')).toBeVisible();
-    await page.locator('#hide-textbox').click();
-    await expect(page.locator('#displayed-text')).toBeHidden();
+    //await expect(page.locator('#displayed-text')).toBeVisible();
+    //await page.locator('#hide-textbox').click();
+    //await expect(page.locator('#displayed-text')).toBeHidden();
+
+    //page.on('dialog',dialog=>dialog.accept()); //listener
+    //await page.locator("#confirmbtn").click();
+
 })
