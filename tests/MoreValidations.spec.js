674e10f2f17691257049c5683cdabe0f4055898a
sathappan1989 <a.s.sathappan@hotmail.com>
2024-12-24 05:13:50 -0700

    PlaywrightScreen Learning

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -22,3 +22,19 @@ test("Popup validations", async({page})=>{
 
     //page.on('dialog',dialog=>dialog.accept()); //listener
     //await page.locator("#confirmbtn").click();
+
+    test("Screenshot validations", async({page})=>{
+        await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+    
+       await expect(page.locator('#displayed-text')).toBeVisible();
+       await page.locator('#displayed-text').screenshot({path:'partialScreenshot.png'});
+       await page.locator('#hide-textbox').click();
+       await page.screenshot({path: 'screenshot.png'});
+       await expect(page.locator("#displayed-text")).toBeHidden();
+
+    })
+
+test.only('visual', async({page})=>{
+    await page.goto("https://www.google.com");
+    expect(await page.screenshot()).toMatchSnapshot('landing.png');
+})
